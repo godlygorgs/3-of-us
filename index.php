f464200a6d3826c19d98ad4ba12c5730d321073a
godlygorgs <kanaka.azalia29@sma.belajar.id>
2026-04-13 15:57:47 +0800

    Convert index.html to index.php

--- index.php
+++ index.php
@@ -225,7 +225,8 @@
       <div class="row">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.104315032492!2d98.87574377407549!3d3.5634529964107604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031485d98f6c317%3A0xbaa0a06c4c929821!2sSMA%20Negeri%201%20Lubukpakam!5e0!3m2!1sen!2sid!4v1774802958695!5m2!1sen!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
           
-        <form action="">
+       
+        <form action="https://3ofus.infinityfreeapp.com/kontak.php" method="POST">
           <div class="input-group">
             <i data-feather="user"> </i>
             <input type="text" placeholder="nama">
